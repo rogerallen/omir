cd9224b387f5ed21fc615f361db20c49e867aaf4
Roger Allen <rallen@gmail.com>
2014-09-17 23:23:24 -0700

    tweak guitar

--- src/omir/synth.clj
+++ src/omir/synth.clj
@@ -11,12 +11,14 @@
   (let [s (o/at t (piano/sampled-piano note))]
     (o/at (+ t dur) (o/ctl s :gate 0))))
 
-(def mpp-ektara (partial oss/ektara :gate 1))
+(def mpp-ektara (partial oss/ektara :gate 1
+                         :noise-amp 0.9 :lp-freq 4000 :lp-rq 0.7))
 (defn play-ektara
   [t note dur]
   (let [s (o/at t (oss/ektara note 1))]
     (o/at (+ t dur) (o/ctl s :gate 0))))
 
+(def mpp-overpad osynth/overpad)
 (defn play-overpad
   [t note dur]
   (let [s (o/at t (osynth/overpad note))]
